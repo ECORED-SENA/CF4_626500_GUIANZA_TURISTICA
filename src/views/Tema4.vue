c06d7a04d82acf4ff530ea0b040677755baed490
elrilacho <80546125+elrilacho@users.noreply.github.com>
2022-09-07 13:11:25 -0500

    Tema 4 listo

---
+++ src/views/Tema4.vue
@@ -0,0 +1,189 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Técnicas de redacción creativa
+    .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema2/1.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Para abordar las técnicas de redacción creativa, es importante primero poder abordar algunos conceptos, reglas y recomendaciones que nos permitirán tener un mejor desarrollo de los contenidos a redactar y las cuales desarrollaremos.
+    .tarjeta.color-primario.rounded-pill.p-3.d-inline-block.mb-5
+      h4.mb-0 Gramática y semántica 
+    p.mb-5(data-aos='fade-right') Igual que cualquier otro texto, la narración requiere del seguimiento de reglas y normas de gramática y semántica para darle calidad al producto creativo. 
+    .cajon.color-primario(style='background-color: #ffd667').p-4.mb-4
+      p.mb-0 La gramática es lo que se define como las reglas ortográficas y que dan claridad y corrección a los textos o al habla. Y la semántica, es la que permite identificar los patrones lingüísticos, los temas y atributos del escrito permitiendo la simplificación de este y por ende la comprensión de lo que leemos o escuchamos.  Todos los idiomas cuentan con las reglas o instrucciones válidas para cada una, esta es la sintáctica y tiene que ver con el componente gramatical de escribir correctamente las palabras y al significado que le encontramos a este orden, se le define como semántica.
+    .row.justify-content-center.mb-5
+      .col-lg-6: img(src='@/assets/curso/temas/tema4/2.jpg', alt='')
+      .col-lg-6 
+        p.mb-5(data-aos='fade-right') 
+          span.r--3 La semántica tiene dos componentes llamados denotación, es decir el significado de las palabras, el sentido de lo oficial que es evidente por el diccionario y el de connotación que dependerá del lenguaje coloquial o popular como parte de un costumbrismo.  Como la semántica se basa en estructuras lingüísticas puede tener conceptos asociados a la lógica, la cognición y por supuesto al lenguaje en donde la semántica es una cuestión de significado, definida por la sintaxis y la pragmática. 
+          br 
+          br
+          | Es importante de acuerdo con el idioma y el público objetivo, tener en cuenta las normas lingüísticas acompañadas de las técnicas de redacción hacen que el lenguaje oral o verbal, sea comprensible y cumpla con el objetivo comunicativo según la intención del mensaje.
+    p.mb-5(data-aos='fade-right') En las técnicas de redacción definir o caracterizar las diferencias entre narrar, exponer, argumentar, dialogar o resumir.  La precisión, la prudencia y la empatía son necesarias para el buen desarrollo de la escritura.  Para escribir mejor se debe empezar por obtener un alto nivel de lectura, lo cual se desarrolla con el hábito, además de esto, se debe tener en cuenta:
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        ul.lista-ul--color
+          li
+            i.vineta 
+            | Adquiera el hábito de escribir y corregir los textos.
+          li
+            i.vineta 
+            | Escribir guardando las normas ortográficas.  
+          li
+            i.vineta 
+            | Leer lo que se escribe identificando tipos, cacofonías y otros errores sencillos para corregirlos.
+          li
+            i.vineta 
+            | No hay que confiarse de los correctores de textos, hay que revisar varias veces. 
+
+          li
+            i.vineta 
+            | La revisión debe incluir puntuación. 
+          li
+            i.vineta 
+            |  Tenga en cuenta no usar palabras en otro idioma si estas existen en español.
+          li
+            i.vineta 
+            | Estar al tanto de nuevas palabras y las que van en desuso o desaparecen, consultando siempre en la Real Academia Española (RAE)
+      .col-lg-6: img(src='@/assets/curso/temas/tema4/3.jpg', alt='')
+    .tarjeta.color-primario.rounded-pill.p-3.d-inline-block.mb-5
+      h4.mb-0 Las normas ortográficas
+    .row.justify-content-center.mb-5
+      .col-lg-1.col-6.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema3/2.svg', alt='')
+      .col-lg-11 
+        p Corresponde a las correcciones para tener en cuenta en la escritura.  Estas reglas y su uso dan calidad y claridad textual. A continuación, veremos algunas de las normas básicas de la RAE para una ortografía básica para tener en cuenta.
+          br
+          br
+          |Si bien hay muchas normas para conocer y desarrollar veamos a continuación algunas sobre acentuación y el uso de correcto de algunas letras, comencemos con las normas de acentuación:
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Tabla 2.
+      span Normas de acentuación RAE (2010)
+    .tabla-a.tabla-editada.mb-5
+      table
+        thead
+          tr
+            th.fw-bold.text-center(rowspan='3') Acentuación
+            
+        tbody
+          tr
+            td.text-center Solo
+            td.text-center No debe tener tilde nunca cuando no hay riesgo de ambigüedad. Cuando hay riesgo de ambigüedad y es adverbio, se desaconseja la tilde y en su lugar se prefieren las formas sinónimas únicamente o solamente.
+            td Solo adultos
+              br
+              | Metió solamente un gol
+
+          tr
+            td.text-center Este, esta, estos, estas, ese, esa, esos, esas, aquel, aquella, aquellos, aquellas
+            td.text-center No debe tener tilde nunca cuando no hay riesgo de ambigüedad. Cuando hay riesgo de ambigüedad y es pronombre, se desaconseja la tilde.
+            td Esta es bonita, aunque prefiero aquella
+          tr
+            td.text-center Monosílabos
+            td.text-center Los monosílabos ortográficos no se tildan nunca, salvo en los casos de tilde diacrítica. En concreto, ya no se tildan las palabras en las que todas sus vocales forman un diptongo o un triptongo ortográfico (antes se admitía como excepción).
+            td Guion, Sion, ion, prion, truhan, frio, friais, lie, lio, liais, hui, huis (y no guión, Sión, ión, prión, truhán, frió, friáis, lié, lió, liáis, huí, huís) Con voseo: fias, lia (y no fiás, liá)
+
+          tr
+            td.text-center o entre cifras
+            td.text-center No se tilda nunca, pues el riesgo de confundir la letra o con el 0 (cero) es mínimo. Recuérdese que ante un número que empieza por o se convierte en u.
+            td 4 o 5
+              br
+              |23 o 24
+              br
+              |79 u 80
+
+          1 tr
+            td.text-center ý
+            td.text-center Se acentúa la letra y en nombres de ortografía arcaizante donde tiene el valor de la vocal i, si le corresponde por las normas generales.
+            td Ýñigo, Aýna
+    p.mb-5(data-aos='fade-right') También como indicamos veamos a continuación otras normas ya relacionadas con el uso de determinadas letras.
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Tabla 3.
+      span Normas de acentuación RAE (2010)
+    .tabla-a.tabla-editada.mb-5
+      table
+        thead
+          tr
+            th.fw-bold.text-center(rowspan='3') Letras
+            
+        tbody
+          tr
+            td.text-center h
+            td.text-center En las palabras que se pueden escribir con hache o sin ella, se prefiere siempre la forma sin hache, con independencia del uso.
+            td Ala, ale, alacena, alelí, armonía, arpa, arpillera, atajo, boardilla, baraúnda, desarrapado, ológrafo, sabiondo, uy.
+          tr
+            td.text-center g, j
+            td.text-center En los préstamos en los que la g y la j tiene un sonido parecido al de y, se recomienda adaptar bien la pronunciación, bien la escritura.
+            td banjo, júnior, mánager pronunciados [bánjo], [júnior], [mánajer] banyo, júnior, mánayer.
+          tr
+            td.text-center w
+            td.text-center Se admite la w en préstamos, letra que se considera preferible en las voces en las que su uso está asentado. Por tanto, no necesitan cursiva.
+            td kiwi, sándwich, web, wéstern, taiwanés, kuwaití.
+          tr
+            td.text-center k
+            td.text-center En préstamos, se prefiere conservar la k en las palabras que se usan mayoritariamente con ella.
+            td alaskeño, askenazi, bikini, euskera, harakiri, kamikaze, karst, kimono, kurdo, moka, musaka, póker, uzbeko Irak, Alaska, Marrakech, Pakistán.
+          tr
+            td.text-center c, qu
+            td.text-center En préstamos, se prefiere la forma adaptada con c o qu cuando es mayoritaria en el uso.
+            td caqui, cuáquero, iraquí, neoyorquino, polca, queroseno, quiosco, quiosquero, telequinesia, valquiria Tanganica, Turquestán
+          tr
+            td.text-center qa, qe, qi, qo, qu
+            td.text-center Se recomienda cambiar las combinaciones qa, qe, qi, qo, qu en nombres comunes adaptados y macrotopónimos a ca, que, qui, co, cu, cuando tienen estos valores. En el resto de los nombres, se pueden conservar.
+            td cuórum, exequátur, cuadrivio, cuark, cuásar, catarí Catar (se prefiere a Qatar) Al Qaeda
+          tr
+            td.text-center y final tras vocal 
+            td.text-center Como norma general, se prefiere -ay, -ey, -oy, -uy en estos diptongos a final de palabra, aunque hay excepciones en préstamos donde se escribe -ai, -ei, -oi, -ui. Cuando ambas formas son válidas, se prefiere la mayoritaria en el uso.
+            td carey, yóquey, tepuy, jagüey agnusdéi, saharaui bonsái, paipai, samurái, tipoy
+          tr
+            td.text-center y final tras consonante
+            td.text-center Al adaptar extranjerismos que terminan en consonante + y, esta se transforma en i. También se escriben con i latina final los hipocorísticos.
+            td bodi, curri, ferri, panti, rali, sexi Mari, Cati, Toni, Dani
+          tr
+            td.text-center y medial
+            td.text-center En préstamos, y pasa a i cuando tiene este valor de vocal. Se recuerda que también se cambia al formar el plural de algunas palabras que terminan en vocal + y.
+            td lycra, géiser, aimara gais, yoqueis, jerséis
+          tr
+            td.text-center ng final
+            td.text-center Se recomienda suprimir la g final al adaptar palabras terminadas en -ng. El diccionario académico recoge como casos excepcionales fang, gong y yang.
+            td cáterin, campin, casting, esmoquin, márquetin, piercing, parking, pudin o pudín
+          tr
+            td.text-center ps inicial
+            td.text-center El uso culto sigue prefiriendo la grafía con ps- inicial, aunque en todos los casos se admiten las formas con s-.
+            td psicología, psicosis, psiquiatra, pseudoprofeta, psoriasis, seudología, seudónimo, seudópodo
+          tr
+            td.text-center Derivados de nombres propios
+            td.text-center En los derivados de nombres propios se conservará la escritura del nombre del que procede. Esta norma no se aplica a los nombres comunes que han pasado a designar objetos, enfermedades, productos...
+            td beethoveniano, picassiano, taylorismo, shakespeariano, darwinista pero alzhéimer, boicot, jacuzzi, zepelín o zeppelin.
+    .contenedor(style='background-color: #ffd667').p-4
+      .row.justify-content-center.mb-5
+        .col-lg-1.col-6.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema4/5.svg', alt='')
+        .col-lg-11 
+          p.mb-0 Estas son sólo algunas normas extraídas de la última actualización en ortografía, publicada por la Real Academia Española en 2010. Para poder ver todas e información adicional sobre la ortografía te invitamos a revisar el material complementario relacionado a Técnicas de Redacción Creativa.
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema4',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
---
+++ src/views/Tema6.vue
@@ -0,0 +1,170 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 3
+      h1 Texto narrativo creativo
+    p.mb-5(data-aos='fade-right') Toda expresión escrita que tiene como esencia la creatividad e imaginación pertenece a la familia de la escritura creativa que cuenta con tres recursos narrativos:  descripción, secuencias de acción y diálogo.
+    .row.justify-content-center.mb-5
+      .col-lg-6.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema3/1.jpg', alt='')
+      .col-lg-6
+        p.mb-4 Las formas del texto narrativo que requieren creatividad son variadas, sin importar si se escriben en prosa o verso, cada una tiene espíritu propio, es decir, cuenta con características que la definen, entre ellas se enlistan:  
+        ul.lista-ul--color.ms-5
+          li
+            i.vineta
+            | Las poesías.
+          li
+            i.vineta
+            | Los cuentos. 
+          li
+            i.vineta
+            | El teatro.
+          li
+            i.vineta
+            | Los guiones. 
+          li
+            i.vineta
+            | Las novelas. 
+          li
+            i.vineta
+            | La leyenda
+          li
+            i.vineta
+            | El mito 
+          li
+            i.vineta
+            | La biografía
+          li
+            i.vineta
+            | El periodístico:  crónica, noticia
+        .cajon.color-primario(style='background-color: #FFD667 ').p-4.mb-4
+          p.mb-0 Las historias pueden tener como base la estructura de alguno de los recursos anteriormente mencionados, en importante que el periodo creativo esté acompañado de inspiración y también de técnicas para su desarrollo. 
+    .row.justify-content-center.mb-5
+      .col-lg-1.col-6.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema3/2.svg', alt='')
+      .col-lg-11 
+        p 
+          span.r--3 El guía se motiva a la escritura con base en lecturas de historia, geografía, cultura y otros temas para que el guion o texto que necesite elaborar este acorde con el público objetivo y el contexto en donde va a ser relatada.
+          br
+          br
+          |El conocimiento de las técnicas de escritura, arriesgarse a crear, hacer paneo de emociones y tener creatividad hacen parte de las cualidades para innovar de forma escrita.  
+    .tarjeta.color-primario.rounded-pill.p-3.d-inline-block.mb-5
+      h4.mb-0 Componentes de la narrativa creativa
+    .row.justify-content-center.aling-items-center.mb-5
+      .col-lg-6.mb-lg-0.mb-3
+        p Para el desarrollo de escritura creativa se hace indispensable tener en cuenta el concepto de género pues cada uno tiene una característica que lo define, recordando que, la estructura de un escrito es común a varios géneros, pero que específicamente en la narración se integran los componentes necesarios para que la historia sea contada con calidad y técnica.  
+          br
+          br
+          |Los componentes de la narración los podemos ver a continuación, en la siguiente tabla:
+      .col-lg-6: img(src='@/assets/curso/temas/tema3/3.jpg', alt='')
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Tabla 1.
+      span Componentes con narración
+    .tabla-a.tabla-editada.mb-5
+      table
+        thead
+          tr
+            th.fw-bold.text-center Componentes
+            th.text-center Definición/Función
+            th Consiste en
+        tbody
+          tr
+            td.fw-bold.text-center El Narrador
+            td.text-center No es lo mismo que el autor. Su función es llevar el ritmo de la narración, el que es variado.
+            td - Narrador omnisciente: usa estilo de narración en tercera persona; él conoce a los personajes y sus pensamientos, así como su pasado, presente y futuro y hasta los deseos más íntimos. 
+              br
+              | - Narrador testigo: cuenta la historia como espectador y en ocasiones como protagonista secundario, él estuvo allí como observador y, no necesariamente como participante, utiliza el monólogo interior.  
+              br
+              | - Narrador protagonista: narra la historia como personaje principal, es decir en primera persona, narra los hechos vividos personalmente.
+
+          tr
+            td.fw-bold.text-center Los Personajes
+            td.text-center Seres reales o imaginarios que están en una obra.
+            td - Protagonista o personaje principal de la obra.
+              br
+              |- Antagonista o personaje de oposición al protagonista: lo enfrenta, necesario para que haya conflicto.
+              br
+              |- Secundario: es importante mas no imprescindible, acompaña la acción y ayuda a la realización de la historia.
+              br
+              |- Nominal: corresponde a aquellos personajes de fantasía, son nombrados en el texto sin participación en la trama.
+
+          tr
+            td.fw-bold.text-center Los Tiempos
+            td.text-center Es el referente histórico, de momentos con elementos sociales o culturales 
+            td - Tiempo Cronológico o propio de la historia:  es la línea que corresponde al tiempo real, al paso de las horas, los días; también se da con fechas específicas.
+              br
+              | - Tiempo Psicológico o tiempo del relato: difícil de determinar con claridad cuánto tiempo transcurre. Generalmente ocurre en momentos de angustia o dolor, o al evocar tiempo pasado por un recuerdo y es difícil identificar cuánto tardó dicho recuerdo.
+
+          tr
+            td.fw-bold.text-center La Secuencia
+            td.text-center Hilo narrativo, orden de los hechos
+            td - Lineal: aparece cuando la narración sigue un orden lógico de aparición de los hechos. Por ejemplo: nacer, crecer, morir. Un terremoto, destrucción, auxilio y reconstrucción.
+              br
+              | - Perturbada: este tipo de secuencia se da cuando el orden lógico se encuentra alterado. En el ejemplo anterior, sería si se empieza con la reconstrucción y luego la narración se va devolviendo al momento del terremoto.
+
+          tr
+            td.fw-bold.text-center Los Registros narrativos
+            td.text-center Son los usos del lenguaje que el narrador registra en sus personajes, sirve para dar veracidad al relato 
+            td - Culto: utiliza lenguaje elevado propio de personas letradas.
+              br
+              | - Vulgar: uso de palabras comunes y parte de la sabiduría popular. 
+              br
+              | - El poético: uso de versos y figuras literarias.
+              br
+              | - El técnico: uso de términos propios de alguna profesión.
+              br
+              | - El campesino: uso de palabras costumbristas o propias de su cotidianidad.
+          tr
+            td.fw-bold.text-center Los Estilos Narrativos
+            td.text-center Formas en que el narrador expone lo hablado
+            td Directo: reproduciendo las palabras textuales, tal y como están escritas para el personaje; indicada por la marca textual del guion de diálogo o las comillas. 
+              br
+              | El guion muestra las palabras exactas del diálogo entre los personajes. Las comillas, representan los monólogos o soliloquios y también la citación de palabras exactas de una tercera persona, cuando se requiere.
+              br
+              | Indirecto, se usa cuando se cuenta lo que otro dijo, pensó o murmuró, de manera indirecta. 
+
+          tr
+            td.fw-bold.text-center Los Espacios
+            td.text-center Son los diferentes niveles, sitios o lugares de una obra literaria que se caracteriza por ser pluriespacial. 
+            td - Físico o real: donde se desarrollan los hechos.
+              br
+              | - Ético: los valores o la carencia de ellos según el texto.
+              br
+              | - Social: clases sociales reflejadas en la obra.
+              br
+              | - Económico: descripción estatus económico.
+              br
+              | - Político: papel de la política en el texto.
+              br
+              | - Legislativo: leyes que gobiernan a los personajes.
+              br
+              | - Religioso: creencias, ritos religiosos de algún tipo, estos se analizarán en este espacio.
+              br
+              | - Ecológico: puede ser que una obra describa el contexto natural en el que se dan las acciones, esto permite referirse a la ecología.
+              br
+              | - Hay otros como simbólico, abierto, cerrado y utilitario.
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema6',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
